cfa1de59f1a1c763128e5236d9f2b264fefcbddd
Karthik Ram <100182451+karthikram1269@users.noreply.github.com>
2025-08-22 17:18:03 +0530

    initial commit

---
+++ ZomatoAgentDeliveryService/src/main/java/com/zomato/gk/ZomatoAgentDeliveryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.zomato.gk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoAgentDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoAgentDeliveryServiceApplication.class, args);
+	}
+	
+
+}
---
+++ ZomatoAgentDeliveryService/src/main/java/com/zomato/gk/service/ZomatoAgentDeliveryService.java
@@ -0,0 +1,19 @@
+package com.zomato.gk.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ZomatoAgentDeliveryService {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	
+	public void updateDeleiveryAgentLocation(String string) {
+		
+		kafkaTemplate.send("zomato-delivery-agent", string);
+	}
+
+}
