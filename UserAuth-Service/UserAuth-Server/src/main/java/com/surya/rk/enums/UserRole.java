ee9b330f4a64b4a78b1b34a683b5d8a1581a45ea
Karthik Ram Gudimetla <100182451+karthikram1269@users.noreply.github.com>
2025-08-23 09:59:24 +0530

    Delete UserAuth-Service/UserAuth-Server directory

--- UserAuth-Service/UserAuth-Server/src/main/java/com/surya/rk/enums/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.surya.rk.enums;
-
-public enum UserRole {
-
-	ADMIN,USER,EMPLOYEE,DEVELOPER,TESTER,ORGANISATION
-}
--- UserAuth-Service/UserAuth-Server/src/main/java/com/surya/rk/services/jwt/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.surya.rk.services.jwt;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-
-	UserDetailsService userDetailsService();
-}
